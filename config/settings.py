6630dc3a81ea02432963f3f1213a5fff6b80271f
Shohjahon <110376477+Shohjahon0407@users.noreply.github.com>
2026-03-31 18:53:02 +0500

    fix BASE_DIR path error

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,10 @@ from pathlib import Path
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
